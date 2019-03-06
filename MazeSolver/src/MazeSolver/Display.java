f68d12070a90ac395cb1f48175163b180dd56c24
JEC845 <JEC845@student.bham.ac.uk>
2019-03-06 17:22:21 +0000

    GUI now opens upon construction of a Display object

--- MazeSolver/src/MazeSolver/Display.java
+++ MazeSolver/src/MazeSolver/Display.java
@@ -29,105 +29,146 @@ import javax.swing.SwingConstants;
 
 public class Display {
 	
+	private final int GRID_WIDTH = 18;
+	private final int GRID_HEIGHT = 12;
+	
 	private boolean activeButtons;
 
 	private JFrame frmEvMazeSolver;
-	private JComponent[][] grid = new JComponent[18][12];
-	private JButton[][] cells = new JButton[9][6];
+	private JComponent[][] grid = new JComponent[GRID_HEIGHT][GRID_WIDTH];
+	//private JButton[][] cells = new JButton[6][9];
 
 	/**
-	 * Launch the application.
+	 * Main method for testing purposes only
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Display window = new Display();
-					window.frmEvMazeSolver.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		Display window = new Display();
+		int[][] map = new int[][] {
+			
+			{  1, -1,  0, -1, -1, -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+			{  1, -1,  0, -1,  1, -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+			{  1, -1, -1, -1,  1, -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+			{  1, -1, -1, -1,  1, -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+			{  1,  1,  1,  1,  1, -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+			{ -1, -1, -1, -1, -1, -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+			{  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+			{  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+			{  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+			{  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+			{  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+			{  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+		};
+		window.update(map);
 		
 	}
 	
-	
 	/**
 	 * Update the display to have the correct colours.
-	 * @param map A 2D array representation of the map status (see CustomOccupancyMap).
+	 * @param map 
+	 * A 2D array representation of the map status (see CustomOccupancyMap).
 	 */
 	public void update(int[][] map) {
-		for(int i = 0; i < 12; i++) {
-			for(int j = 0; j < 18; i++) {
-				grid[i][j].setBackground(getColour(map[i][j]));
+		for(int i = 0; i < GRID_HEIGHT; i++) {
+			for(int j = 0; j < GRID_WIDTH; j++) {
+				grid[i][j].setBackground(getBGColour(map[i][j]));
+				grid[i][j].setForeground(getFGColour(map[i][j]));
 			}
-			
-		}
-			
+		}		
 	}
 	/**
-	 * Get the correct colour for the grid square.
-	 * @param state The state of the grid square (-1 occupied, 0 unknown, 1, clear).
-	 * @return The colour for the GUI to display for this state.
+	 * Get the correct background colour for the grid square.
+	 * @param state 
+	 * The state of the grid square (-1 occupied, 0 unknown, 1, clear).
+	 * @return 
+	 * The colour for the GUI to display for this state.
 	 */
-	public static Color getColour(int state) {
+	public static Color getBGColour(int state) {
 			if (state == -1) {
-				return Color.LIGHT_GRAY;
+				return Color.BLACK;
 			}
 			if (state == 0) {
-				return Color.WHITE;
+				return Color.LIGHT_GRAY;
 			}
 			if (state == 1) {
-				return Color.BLACK;
+				return Color.WHITE;
 			}
 			else return Color.ORANGE;
 			
 		}
 	/**
-	 * Create the application, with buttons inactive.
+	 * Get the correct foreground colour for the grid square.
+	 * @param state 
+	 * The state of the grid square (-1 occupied, 0 unknown, 1, clear).
+	 * @return 
+	 * The colour for the GUI to display for this state.
+	 */
+	public static Color getFGColour(int state) {
+			if (state == -1) {
+				return Color.WHITE;
+			}
+			if (state == 0) {
+				return Color.BLACK;
+			}
+			if (state == 1) {
+				return Color.BLACK;
+			}
+			else return Color.BLACK;
+			
+		}
+	/**
+	 * Create the application with buttons inactive.
 	 */
 	public Display() {
-		activeButtons = false;
+		this.activeButtons = false;
 		initialize();
+		frmEvMazeSolver.setVisible(true);
+		//cells[0][0].setBackground(Color.BLUE);
 	}
 	
 	/**
 	 * Create the application.
-	 * @param active Whether or not the buttons should be active.
+	 * @param active 
+	 * Whether or not the buttons should be active.
 	 */
 	public Display(boolean active) {
-		activeButtons = active;
+		this.activeButtons = active;
 		initialize();
+		frmEvMazeSolver.setVisible(true);
 	}
 	
 	/**
 	 * Get the grid.
-	 * @return The 2D array containing all swing components on the grid.
+	 * @return 
+	 * The 2D array containing all swing components on the grid.
 	 */
 	public JComponent[][] getGrid() {
 		return grid;
 	}
 	/**
 	 * Get a specific swing component from the grid.
-	 * @param x x coordinate.
-	 * @param y y coordinate.
-	 * @return swing component at this location.
+	 * @param x 
+	 * x coordinate.
+	 * @param y 
+	 * y coordinate.
+	 * @return 
+	 * Swing component at this location.
 	 */
 	public JComponent getItem(int x,int y) {
 		return grid[x][y];
 	}
 	/**
 	 * Get a specific swing component from the grid.
-	 * @param coords coordinates [x,y].
-	 * @return swing component at this location.
+	 * @param coords 
+	 * Coordinates [x,y].
+	 * @return 
+	 * Swing component at this location.
 	 */
 	public JComponent getItem(int[] coords) {
 		return grid[coords[0]][coords[1]];
 	}
 
 	/**
-	 * Initialize the contents of the frame.
+	 * Initialise the contents of the frame.
 	 */
 	private void initialize() {
 		frmEvMazeSolver = new JFrame();
@@ -153,15 +194,16 @@ public class Display {
 		JButton btnTemp;
 		GridBagConstraints gbc_btnTemp;
 		GridBagConstraints gbc_lblTemp;
-		for (int i=0;i<18;i++) {
-			for (int j=0;j<12;j++) {
+		for (int i=0;i<GRID_HEIGHT;i++) {
+			for (int j=0;j<GRID_WIDTH;j++) {
+				System.out.println("Initialising grid "+i+" "+j);
 				if(i%2 == 0) {
-					if (j%2==1) { //Create a wide label
-						lblTemp = createWall(59,19);
+					if (j%2==1) { //Create a tall label
+						lblTemp = createWall(19,59);
 						gbc_lblTemp = new GridBagConstraints();
 						//gbc_lblTemp.insets = new Insets(0, 0, 0, 5);
-						gbc_lblTemp.gridx = i;
-						gbc_lblTemp.gridy = 11-j;
+						gbc_lblTemp.gridx = j;
+						gbc_lblTemp.gridy = 11-i;
 						pnlGridMap.add(lblTemp, gbc_lblTemp);
 						grid[i][j] = lblTemp;
 						
@@ -176,15 +218,17 @@ public class Display {
 						btnTemp.setPreferredSize(new Dimension(60, 60));
 						btnTemp.setMinimumSize(new Dimension(59, 59));
 						btnTemp.setMaximumSize(new Dimension(59, 59));
-						btnTemp.setBackground(getColour(-1));
+						btnTemp.setBackground(getBGColour(0));
+						btnTemp.setForeground(getFGColour(0));
+						btnTemp.setOpaque(true);
 						gbc_btnTemp = new GridBagConstraints();
 						//gbc_btnTemp.insets = new Insets(0, 0, 5, 5);
-						gbc_btnTemp.gridx = i;
-						gbc_btnTemp.gridy = 11-j;
+						gbc_btnTemp.gridx = j;
+						gbc_btnTemp.gridy = 11-i;
 						pnlGridMap.add(btnTemp, gbc_btnTemp);
 						
-						cells[i/2][j/2] = new JButton("("+String.valueOf(i/2)+","+String.valueOf(j/2)+")");
-						grid[i][j] = cells[i/2][j/2];
+						//cells[i/2][j/2] = new JButton("("+String.valueOf(i/2)+","+String.valueOf(j/2)+")");
+						grid[i][j] = btnTemp;
 						
 						
 					}
@@ -195,18 +239,18 @@ public class Display {
 						lblTemp = createWall(19,19);
 						gbc_lblTemp = new GridBagConstraints();
 						//gbc_lblTemp.insets = new Insets(0, 0, 0, 5);
-						gbc_lblTemp.gridx = i;
-						gbc_lblTemp.gridy = 11-j;
+						gbc_lblTemp.gridx = j;
+						gbc_lblTemp.gridy = 11-i;
 						pnlGridMap.add(lblTemp, gbc_lblTemp);
 						grid[i][j] = lblTemp;
 					}
 					
-					else {// Create a tall label
-						lblTemp = createWall(19,59);
+					else {// Create a wide label
+						lblTemp = createWall(59,19);
 						gbc_lblTemp = new GridBagConstraints();
 						//gbc_lblTemp.insets = new Insets(0, 0, 0, 5);
-						gbc_lblTemp.gridx = i;
-						gbc_lblTemp.gridy = 11-j;
+						gbc_lblTemp.gridx = j;
+						gbc_lblTemp.gridy = 11-i;
 						pnlGridMap.add(lblTemp, gbc_lblTemp);
 						grid[i][j] = lblTemp;
 					}
@@ -222,7 +266,8 @@ public class Display {
 		out.setPreferredSize(new Dimension(width, height));
 		out.setMinimumSize(new Dimension(width, height));
 		out.setMaximumSize(new Dimension(width, height));
-		out.setBackground(getColour(-1));
+		out.setBackground(getBGColour(0));
+		out.setForeground(getFGColour(0));
 		out.setOpaque(true);
 		return out;
 	}
